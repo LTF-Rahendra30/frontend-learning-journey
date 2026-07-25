dd5ae7d16c910f857286b95b6c14d42f0d09b65f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-25 22:04:00 +0700

    try make data type category in variabels

---
+++ 01-fundamentals/01-variabels_data_type/01_data_type.js
@@ -0,0 +1,10 @@
+let name = "Bobby"  // String
+let age = 12 // Number or Integer
+let height = 160.5 // float
+let weight // Undefined
+let married = null // null
+let haveGirlFriends = false // boolean
+
+console.log(`${name} , ${age}, ${height}, ${weight}, ${married}, ${haveGirlFriends}`)
+
+// Output: Bobby , 12, 160.5, undefined, null, false
