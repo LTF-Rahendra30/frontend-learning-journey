84a7eb28fcf87ff8f1624e6b250b088a260a7dc0
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-25 22:06:20 +0700

    try Print data type of variable with 'typeof' atributte

--- 01-fundamentals/01-variabels_data_type/01_data_type.js
+++ 01-fundamentals/01-variabels_data_type/01_data_type.js
@@ -8,3 +8,16 @@ let haveGirlFriends = false // boolean
 console.log(`${name} , ${age}, ${height}, ${weight}, ${married}, ${haveGirlFriends}`)
 
 // Output: Bobby , 12, 160.5, undefined, null, false
+
+// Print data type of variable
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof weight)
+console.log(typeof married)
+console.log(typeof haveGirlFriends)
+// Output:
+// string
+// number
+// undefined
+// object
+// boolean
