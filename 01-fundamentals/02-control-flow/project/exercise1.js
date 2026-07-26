baf524006465b22956f8d2af6a4f2c00d5164f6c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 23:13:12 +0700

    fet: Remove with if else stetment of calculator

--- 01-fundamentals/02-control-flow/project/exercise1.js
+++ 01-fundamentals/02-control-flow/project/exercise1.js
@@ -2,17 +2,28 @@
 
 let num1 = 10
 let num2 = 2
-const result1 = num1 + num2
-const result2 = num1 - num2
-const result3 = num1 * num2
-const result4 = num1 / num2
-const result5 = num1 ** num2
+let operator = "+"
+let result = null
 
-console.log(result1)
-console.log(result2)
-console.log(result3)
-console.log(result4)
-console.log(result5)
+if (operator === "+"){
+    result = num1 + num2
+    console.log(result)
+} else if(operator === "-"){
+    result = num1 - num2
+    console.log(result)
+} else if(operator === "*"){
+    result = num1 * num2
+    console.log(result)
+}else if(operator === "/"){
+    result = num1 / num2
+    console.log(result)
+} else if(operator === "**"){
+    result = num1 ** num2
+    console.log(result)
+}
+else{
+    console.log("Nothing")
+}
 // Output:
 // 12
 // 8
