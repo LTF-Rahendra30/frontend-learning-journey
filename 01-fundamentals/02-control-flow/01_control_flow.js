2d6fa1036c060155761abdb9554d35f03f47db60
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 21:48:13 +0700

    try conditionaling with if else

---
+++ 01-fundamentals/02-control-flow/01_control_flow.js
@@ -0,0 +1,14 @@
+const name = "bob"
+const age = 17
+let legalAge = null
+let gpa = 0
+
+// With If Else
+if (age >= 17){
+    legalAge = true
+} else{
+    legalAge = false
+}
+console.log(legalAge)
+// Output : true
+
