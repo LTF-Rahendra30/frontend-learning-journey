9ed81d5ded148349866f5e2c8c8d4894254bdab4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 21:12:39 +0700

    Try change value in the let variabels

--- 01-fundamentals/01-variabels_data_type/02_variabels.js
+++ 01-fundamentals/01-variabels_data_type/02_variabels.js
@@ -4,8 +4,15 @@ const Married = false  // cant change
 
 
 // Try change CONST variabel
-const GPA = 3.5
-GPA = 4.0
-console.log(GPA)
+// const GPA = 3.5
+// GPA = 4.0
+// console.log(GPA)
 // And then error output like this:
 // TypeError: Assignment to constant variable at Object
+
+// Try change LET variabel type
+let haveGirlFriend ="Noo"
+haveGirlFriend = "Yess"
+console.log(haveGirlFriend)
+// Output:
+// Yess
