c2f7801d9eaea4090c51df84ee3be3afb461a984
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 22:51:57 +0700

    Try all operaators of Math

---
+++ 01-fundamentals/02-control-flow/project/exercise1.js
@@ -0,0 +1,21 @@
+// Calculator with Arithmetic + Assignment
+
+let num1 = 10
+let num2 = 2
+const result1 = num1 + num2
+const result2 = num1 - num2
+const result3 = num1 * num2
+const result4 = num1 / num2
+const result5 = num1 ** num2
+
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
+console.log(result5)
+// Output:
+// 12
+// 8
+// 20
+// 5
+// 100
