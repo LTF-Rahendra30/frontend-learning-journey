6eeab5575ee34cf0fd4cee6b56bf0296247d22ea
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 21:08:46 +0700

    make new file to start learn & try about variabel in JS

---
+++ 01-fundamentals/01-variabels_data_type/02_variabels.js
@@ -0,0 +1,11 @@
+var name = "Boby"
+let age = 12 // can change
+const Married = false  // cant change
+
+
+// Try change CONST variabel
+const GPA = 3.5
+GPA = 4.0
+console.log(GPA)
+// And then error output like this:
+// TypeError: Assignment to constant variable at Object
