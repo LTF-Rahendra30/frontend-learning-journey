d42d4d9cc92234d17fc621198be85a6b0decbb28
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-26 22:04:24 +0700

    Try Switch case conditional

--- 01-fundamentals/02-control-flow/01_control_flow.js
+++ 01-fundamentals/02-control-flow/01_control_flow.js
@@ -1,7 +1,6 @@
 const name = "bob"
 const age = 17
 let legalAge = null
-let gpa = 0
 
 // With If Else
 if (age >= 17){
@@ -12,3 +11,36 @@ if (age >= 17){
 console.log(legalAge)
 // Output : true
 
+// With Switch Case
+const day = null
+
+switch(day){
+    case "Monday":
+        console.log("Time to work")
+        break
+    case "Tuesday":
+        console.log("Time to work")
+        break
+    case "Wedneday":
+        console.log("Time to work")
+        break
+    case "Friday":
+        console.log("Time to work")
+        break
+    case "Monday":
+        console.log("Time to work")
+        break
+    case "Sunday":
+        console.log("Holiday")
+        break
+    case "Suturday":
+        console.log("Holiday")
+        break
+    default:
+        console.log("Nothing")
+        break
+}
+
+// day = null ==> "Nothing"
+// day = "Monday - Friday" ==> "Time to work"
+// day = "Sunday - Saturday" ==> "Holiday"
