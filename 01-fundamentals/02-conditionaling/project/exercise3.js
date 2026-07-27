accabe7f8510d9ba5806ac4a790110ccbf94cb6b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-27 20:43:59 +0700

    feat: Make code script for the exercise

--- 01-fundamentals/02-conditionaling/project/exercise3.js
+++ 01-fundamentals/02-conditionaling/project/exercise3.js
@@ -1,1 +1,18 @@
 //  ========= Discount Calculator (Arithmetic + Assignment + Logical) ========
+
+const price = 10000;
+const isMember = true;
+const minPrice = 1000
+let discount = 0;
+
+if (price >= minPrice && isMember === true){
+    discount = price * 0.10;
+} else if (price >= minPrice && isMember === false){
+    discount = price *0.05;
+} else{
+    discount = 0;
+}
+
+const finalPrice = price - discount;
+
+console.log(`Price: ${price}. After Discount: ${finalPrice}`)
