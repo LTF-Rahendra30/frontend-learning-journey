bd278b2ff9bac386ad619e2cadcc73ea5f2d4571
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-27 23:39:27 +0700

    feat: write script code for the exercise

--- 01-fundamentals/02-conditionaling/project/exercise4.js
+++ 01-fundamentals/02-conditionaling/project/exercise4.js
@@ -1,3 +1,20 @@
 //  ============== Login Validator (Comparison + Logical + Nullish Coalescing) =============
 
 
+let username = "bo";
+let password = "129900";
+let statusLogin;
+
+username = username ?? "";
+password = password ?? "";
+
+if (!password || !username){
+    statusLogin = "Username & Password cant be empty";
+} else if(username.length < 3){
+    statusLogin = "Username must be >= 3 Char";
+} else if (password.length < 6){
+    statusLogin = "Password must be >= 6 Char";
+} else{
+    statusLogin = "Login Successed";
+}
+console.log(statusLogin)
