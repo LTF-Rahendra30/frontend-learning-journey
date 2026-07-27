c598fc55837390f3e4f648fa6fe893597a377a62
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-27 21:06:54 +0700

    refactor: added simplyfy with function

--- 01-fundamentals/02-conditionaling/project/exercise3.js
+++ 01-fundamentals/02-conditionaling/project/exercise3.js
@@ -11,8 +11,25 @@ if (price >= minPrice && isMember === true){
     discount = price *0.05;
 } else{
     discount = 0;
+    
 }
 
 const finalPrice = price - discount;
 
-console.log(`Price: ${price}. After Discount: ${finalPrice}`)
+console.log(`Price: ${price}.Discount: ${discount}.  After Discount: ${finalPrice}`);
+
+// --- Simplyfiy with function ----
+
+function Discount(price,isMember){
+    let finalPrice;
+    if (price >=1000 && isMember === true){
+        finalPrice = price - (price * 0.1);
+    } else if (price>=1000 && isMember === false){
+        finalPrice = price - (price * 0.05);
+    } else{
+        return false;
+    }
+    return finalPrice; 
+}
+
+console.log(Discount(10000,true));
