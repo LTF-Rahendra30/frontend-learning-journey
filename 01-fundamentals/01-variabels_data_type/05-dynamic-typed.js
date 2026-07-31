703e42ddebb7a2ad39125ab5c78b1e41a88fa1b5
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-31 15:36:12 +0700

    feat: Add dynamic typing example in JavaScript

---
+++ 01-fundamentals/01-variabels_data_type/05-dynamic-typed.js
@@ -0,0 +1,13 @@
+let something = "Hello";
+console.log(typeof something)
+something = 100;
+console.log(typeof something)
+something = false;
+console.log(typeof something)
+something = null;
+console.log(typeof something)
+
+// Output: string
+/* number
+boolean
+object */
