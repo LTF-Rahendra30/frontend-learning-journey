a3a47ef15b044811d4cb33074037c3c00fde6325
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-31 20:49:04 +0700

    Try make simple array

---
+++ 01-fundamentals/03-data-structure/02-array.js
@@ -0,0 +1,8 @@
+let country = ["Indonesian","Japan","Korean","Malaysia"];
+
+console.log(country);
+// [ 'Indonesian', 'Japan', 'Korean', 'Malaysia' ]
+
+// With index 
+console.log(country[0],country[1]);
+// Indonesian Japan
