03b2ebb10cf26aed4278ae916b653fac5bce4d54
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-31 15:50:31 +0700

    feat: Update object properties and demonstrate access in JavaScript

--- 01-fundamentals/03-data-structure/01-object.js
+++ 01-fundamentals/03-data-structure/01-object.js
@@ -4,6 +4,17 @@ let myWife = {
     haveMarried : true,
     from:undefined
 }
+
 // Update object property:
 myWife.from = "Indonesian";
 console.log(myWife.from)
+// Indonesian
+
+// Object Access prperty:
+console.log(myWife['name']);
+// Chloe
+
+// Update property
+myWife['name'] = "Chloe-un";
+console.log(myWife['name']);
+// Chloe-un
