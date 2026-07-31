1fec6234709416ac26d36fadf79ce4f5334618e8
LTF-Rahendra30 <kecuu91@gmail.com>
2026-07-31 15:32:08 +0700

    feat:  Try all Data type

---
+++ 01-fundamentals/01-variabels_data_type/04-data-type.js
@@ -0,0 +1,33 @@
+// ========= DATA TYPE JAVASCRIPT ===========
+
+let name = "Chloe";
+let age = 18;
+let haveBoyFriend = true;
+let favoritColor = null;
+let from;
+const abbility = Symbol();
+
+const behavior = [
+    "Feminime",
+    "Excited",
+    "Cering",
+]
+
+const favoriteFood = {
+    food : ["Pasta", "Steak", "Ramen","Shusi"],
+    drink : ["Matcha", "Strabberry"]
+}
+
+console.log(typeof name,typeof age, typeof haveBoyFriend, typeof favoritColor, typeof from, typeof abbility, typeof behavior, typeof favoriteFood)
+
+// Output:
+/* 
+string
+number
+boolean
+object
+undefined
+symbol 
+object 
+object
+ */
