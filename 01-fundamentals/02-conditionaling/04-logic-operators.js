608501f93e1e37e27b0f5e24adc59d578511f4ac
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-02 00:00:04 +0700

    try And operator

--- 01-fundamentals/02-conditionaling/04-logic-operators.js
+++ 01-fundamentals/02-conditionaling/04-logic-operators.js
@@ -1,7 +1,17 @@
 
 //  --- OR ---
 let age = 18;
-let healthIndex = 200;
+let healthIndex = 20;
 
-const eligible = age >=21 ||Index >= 150;
+const eligible = age >=21||healthIndex >= 150;
 console.log(eligible);
+// False
+
+
+// --- AND ---
+let name1 = true;
+let name2 = true;
+
+const andOperator = name1 === true && name2 === true;
+console.log(andOperator);
+// Ture
