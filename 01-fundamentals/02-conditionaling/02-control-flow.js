e49127f910b165aa226988e3d40fbd1906714d93
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-01 23:41:21 +0700

    Try Equality Opertor

--- 01-fundamentals/02-conditionaling/02-control-flow.js
+++ 01-fundamentals/02-conditionaling/02-control-flow.js
@@ -4,3 +4,13 @@ let num2 = 12;
 const isGreather = num1 < num2;
 console.log(isGreather);
 //  true
+
+// ===== Operator Equality ====
+let a = 2;
+let b = '2';
+console.log(a == b);
+// true
+
+// ========= Strong Selective Equality ========
+console.log(a === b);
+// false
