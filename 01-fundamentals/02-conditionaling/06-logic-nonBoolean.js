a26639fc686abfb997e3f6ad960e1c26d8fec6cc
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-02 18:16:19 +0700

    Try logic non boolean in 3 condtitional

--- 01-fundamentals/02-conditionaling/06-logic-nonBoolean.js
+++ 01-fundamentals/02-conditionaling/06-logic-nonBoolean.js
@@ -3,3 +3,12 @@ let defultColor = "White";
 
 const currentWebsiteColor = usersChosenColor || defultColor;
 console.log(currentWebsiteColor);
+// White
+
+// 3 Conditional, with first is False,Null, Undefined
+let userTheme = false;
+let defaultTheme = "Dark";
+let systemTheme = "Light";
+const currentTheme = userTheme || defaultTheme || systemTheme;
+console.log(currentTheme);
+// Dark
