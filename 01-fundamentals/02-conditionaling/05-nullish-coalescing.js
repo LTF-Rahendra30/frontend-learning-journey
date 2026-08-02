5afb1be3ddd3c672bfb6ac56b5c2ebf39a3e26d8
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-02 17:47:01 +0700

    Try nullish coalescing operator

---
+++ 01-fundamentals/02-conditionaling/05-nullish-coalescing.js
@@ -0,0 +1,9 @@
+// ============ Nullish Coalescing Operator =============
+
+let name = null;
+const result = name ?? false;
+// which returns its right-hand side operand when its left-hand side operand is null or undefined
+
+
+console.log(result);
+// Flase
