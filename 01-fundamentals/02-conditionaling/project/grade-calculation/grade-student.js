e3c893581780012de8a7926fc3bcdbd819a17818
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-03 21:04:13 +0700

    refactor:  Update validation value + new component (grade + bagde)

--- 01-fundamentals/02-conditionaling/project/grade-calculation/grade-student.js
+++ 01-fundamentals/02-conditionaling/project/grade-calculation/grade-student.js
@@ -3,6 +3,9 @@
 let gradeValue = prompt("Enter your GPA: ");
 // Checking
 let validInput = gradeValue && !isNaN(gradeValue);
+gradeValue = gradeValue ?? 0;
+let bagde;
+let grade;
 
 if (!validInput){
     alert("Enter something!");
@@ -10,10 +13,16 @@ if (!validInput){
     // Convert 
     gradeValue = Number(gradeValue);
     if (gradeValue >= 90){
-        alert("Your Valid");
-    } else if (gradeValue < 90 && gradeValue >= 20){
-        alert("Great");
-    } else if (gradeValue >=0){
-        alert("Remedial");
+        bagde = "Top Performer";
+        grade = "A";
+        alert(`Grade: ${grade}, Bagde: ${bagde}`);
+    } else if (gradeValue < 90 && gradeValue >= 80){
+        grade = "B";
+        alert(`Grade: ${grade}`);
+    } else if (gradeValue < 80 && gradeValue >=70){
+        grade = "C";
+        alert(`Grade: ${grade}`)
+    }else if (gradeValue < 70){
+        alert("Remedial")
     }
 }
