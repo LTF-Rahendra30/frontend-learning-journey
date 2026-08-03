1ec977ab294305e6d358a2a8467e82fcabf9a742
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-03 12:12:27 +0700

    feat: Added logical condituional for grade value

--- 01-fundamentals/02-conditionaling/project/grade-calculation/grade-student.js
+++ 01-fundamentals/02-conditionaling/project/grade-calculation/grade-student.js
@@ -1,7 +1,14 @@
 // ============ INPUT GRADE FROM USER =======
 
 let gradeValue = prompt("Enter your GPA: ");
+gradeValue = gradeValue ?? "";
 
-if (gradeValue >= 0){
-    alert("Your Valid")
+if (gradeValue >= 90){
+    alert("Your Valid");
+} else if (gradeValue < 90 && gradeValue > 20){
+    alert("Great");
+}else if(gradeValue <= 20 && gradeValue >= 0){
+    alert("Remedial!");
+}else {
+    alert("Enter something!");
 }
