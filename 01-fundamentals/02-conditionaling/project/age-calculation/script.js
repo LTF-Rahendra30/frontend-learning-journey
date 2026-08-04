d6c9c16d065a076d9a6e9e3d017334c980aa50a5
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-04 23:58:48 +0700

    feat: Added script JS (get input value & input data)

---
+++ 01-fundamentals/02-conditionaling/project/age-calculation/script.js
@@ -0,0 +1,10 @@
+
+const form = document.getElementById("form-container");
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    // ==== GET RESULT ====
+    const resultContainer = document.getElementById("result");
+    const result = document.getElementById("result-text");
+});
