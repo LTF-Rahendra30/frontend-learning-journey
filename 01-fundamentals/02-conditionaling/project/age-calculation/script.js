278ed4ee78fdc0ff010f856f5348d5ae71bd1b15
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-05 12:26:26 +0700

    feat: change css structure for flexible with main class style and specifig class for style color (Valid & Invalid)

--- 01-fundamentals/02-conditionaling/project/age-calculation/script.js
+++ 01-fundamentals/02-conditionaling/project/age-calculation/script.js
@@ -12,9 +12,7 @@ form.addEventListener('submit', function(event){
     result.classList.remove("result-content-valid") && result.classList.remove("result-content-invalid");
     resultContainer.classList.remove("result-container");
 
-
     // ==== Process ====
-
     // --- Username ---
     const nameInput = document.getElementById("name-input");
     const userName = nameInput.value;
@@ -23,17 +21,16 @@ form.addEventListener('submit', function(event){
     const ageInput = document.getElementById("age-input");
     const userAge = ageInput.valueAsNumber;
     
-
     // ==== Output Pop-up & AGE VALIDATOR ====
     if (userAge >= 17){
         result.textContent = `✅ ${userName}, ${userAge} yo, You're Eligible!`;
+        result.classList.add("result-content");
         result.classList.add("result-content-valid");
         resultContainer.classList.add("result-container");
     }else{
         result.textContent = `❌ You're not Eligible!`
+        result.classList.add("result-content");
         result.classList.add("result-content-invalid");
         resultContainer.classList.add("result-container");
     }
-
-
 });
