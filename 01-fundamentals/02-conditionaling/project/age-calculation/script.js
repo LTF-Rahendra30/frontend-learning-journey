1c5e3369d089b2f4e048d0a6dda1968ae87c0107
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-05 12:15:52 +0700

    feat: Added style with class in html & JS for True condition + False condition

--- 01-fundamentals/02-conditionaling/project/age-calculation/script.js
+++ 01-fundamentals/02-conditionaling/project/age-calculation/script.js
@@ -8,6 +8,10 @@ form.addEventListener('submit', function(event){
     const resultContainer = document.getElementById("result");
     const result = document.getElementById("result-text");
 
+    // ---- Remove
+    result.classList.remove("result-content-valid") && result.classList.remove("result-content-invalid");
+    resultContainer.classList.remove("result-container");
+
 
     // ==== Process ====
 
@@ -20,9 +24,16 @@ form.addEventListener('submit', function(event){
     const userAge = ageInput.valueAsNumber;
     
 
+    // ==== Output Pop-up & AGE VALIDATOR ====
     if (userAge >= 17){
         result.textContent = `✅ ${userName}, ${userAge} yo, You're Eligible!`;
+        result.classList.add("result-content-valid");
+        resultContainer.classList.add("result-container");
     }else{
         result.textContent = `❌ You're not Eligible!`
+        result.classList.add("result-content-invalid");
+        resultContainer.classList.add("result-container");
     }
+
+
 });
