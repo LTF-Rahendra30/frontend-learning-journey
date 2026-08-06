614a46145ce44bc310a15fb67cf3e1acd28d5bd9
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-06 16:37:04 +0700

    refactor: Change and delete duplicate code for JS scripts (in validator If-Else)

--- 01-fundamentals/02-conditionaling/project/age-calculation/script.js
+++ 01-fundamentals/02-conditionaling/project/age-calculation/script.js
@@ -33,42 +33,28 @@ form.addEventListener('submit',(event) => {
     nameRowContent.classList.remove("name-row");
     ageRowContent.classList.remove("age-row");
 
+
+    // ==== Row Content ====
+    nameRowContent.classList.add("row-atribute");
+    ageRowContent.classList.add("row-atribute");
+    // --- Row Information Content ---
+    nameValue.textContent = `Name: ${userName}`;
+    ageValue.textContent = `Age: ${userAge}`;
+    
+    // ==== Container ====
+    document.body.classList.add("popup-active");
+    resultContainer.classList.add("result-container");
+    
     // ==== Output Pop-up & AGE VALIDATOR ====
-    if (userAge >= 17){
-        // ==== Row Content ====
-        nameRowContent.classList.add("row-atribute");
-        ageRowContent.classList.add("row-atribute");
-        
-        // --- Row Information Content ---
-        nameValue.textContent = `Name: ${userName}`;
-        ageValue.textContent = `Age: ${userAge}`;
-        
-        result.textContent = `✅ ${userName}, ${userAge} yo, You're Eligible!`;
-        result.classList.add("result-content");
+    if (userAge >= 17){      
+        result.textContent = `✅ Congratulations You're Eligible!`;
         result.classList.add("result-content-valid");
         
-        // ==== Container ====
-        document.body.classList.add("popup-active");
-        resultContainer.classList.add("result-container");
-        
-    }else{
-        
-        // ==== Row Content ====
-        nameRowContent.classList.add("row-atribute");
-        ageRowContent.classList.add("row-atribute");
-        
-        // --- Row Information Content ---
-        nameValue.textContent = `Name: ${userName}`;
-        ageValue.textContent = `Age: ${userAge}`;
-        
-        result.textContent = `❌ You're not Eligible!`
-        result.classList.add("result-content");
+    }else{         
+        result.textContent = `❌ You're not Eligible!`;
         result.classList.add("result-content-invalid");
-        
-        // ==== Container ====
-        document.body.classList.add("popup-active");
-        resultContainer.classList.add("result-container");
     }
+    result.classList.add("result-content");
     
 });
 
