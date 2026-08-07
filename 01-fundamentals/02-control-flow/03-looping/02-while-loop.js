81d41df55b15d8c6b8f503ba58e3d9570fc0d6a9
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 16:32:58 +0700

    feat: Make new file to try While Loop

---
+++ 01-fundamentals/02-control-flow/03-looping/02-while-loop.js
@@ -0,0 +1,15 @@
+let numbers = [1,2,3,4,5,6,7,8];
+let index = 0;
+
+while(index < numbers.length){
+    console.log(numbers[index]);
+    index ++;
+    /* 1
+    2
+    3
+    4
+    5
+    6
+    7
+    8 */
+}
