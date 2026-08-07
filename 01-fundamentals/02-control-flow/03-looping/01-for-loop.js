d704419278a60a4f9799ceee3533fe00cc49710d
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 16:27:01 +0700

    Try looping number 1 to 100 with for loop

--- 01-fundamentals/02-control-flow/03-looping/01-for-loop.js
+++ 01-fundamentals/02-control-flow/03-looping/01-for-loop.js
@@ -6,8 +6,8 @@ let numbers = [1,2,3,4,5,6,7,8,9];
 // console.log(numbers[index]);
 // // 1
 
-for (let index = 0; index <= numbers.length; index++){
-    console.log(index);
+for (let index = 0; index < numbers.length; index++){
+    console.log(numbers[index]);
     /* 
     0
     1
@@ -19,4 +19,13 @@ for (let index = 0; index <= numbers.length; index++){
     7
     8
     9 */
+}
+console.log("\n For loop beetwen number 1 to 100 \n");
+
+
+// Example For loop beetwen number 1 to 100
+
+for (let num = 1; num <= 100; num++){
+    console.log(num);
+    
 }
