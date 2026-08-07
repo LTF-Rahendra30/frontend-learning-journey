d6afb7ae6548990f04298a05478205334c4625fc
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 19:28:56 +0700

    Try Access property in objbect without Loops

---
+++ 01-fundamentals/02-control-flow/03-looping/04-for-in-loops.js
@@ -0,0 +1,16 @@
+const user ={
+    name : "bob",
+    age: 12,
+    isMarry : false
+};
+
+// Without Loop for access property in the Object
+console.log(user.name);
+console.log(user['age']);
+console.log(user.isMarry);
+// bob
+// 12
+// false
+
+
+// ==== With Loop ===
