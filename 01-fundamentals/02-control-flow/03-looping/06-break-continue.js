38c8680598787fcfc791819644b32e249cdf546f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 19:44:55 +0700

    feat: Added new file to start try Break and Continue in Loop

---
+++ 01-fundamentals/02-control-flow/03-looping/06-break-continue.js
@@ -0,0 +1,15 @@
+
+//  ==== Break ====
+
+for (let i =0; i <10; i++){
+    if(i === 5){
+        break  // to Exits the loop when i = 5
+    }
+    /* 0
+    1
+    2
+    3
+    4
+     */
+    console.log(i);
+}
