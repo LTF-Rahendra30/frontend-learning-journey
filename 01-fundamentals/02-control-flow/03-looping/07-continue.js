0992ceb4c42080cbc7bbba2eea369cec6c684a07
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 20:06:45 +0700

    feat: Added new file to try Continue stetment in for loops

---
+++ 01-fundamentals/02-control-flow/03-looping/07-continue.js
@@ -0,0 +1,12 @@
+for(let i = 0; i<10; i++){
+    if (i %2 === 0){
+        continue; // skipp value of i that  have modulo of 2, Or Even Number
+    }
+    console.log(i);
+    /* 
+    1
+    3
+    5
+    7
+    9 */
+}
