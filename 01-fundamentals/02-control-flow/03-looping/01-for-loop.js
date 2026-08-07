595ce3666cc13f5721f635f3dbfb8049927ed876
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-07 16:22:15 +0700

    Try for loop with indexing of Array

--- 01-fundamentals/02-control-flow/03-looping/01-for-loop.js
+++ 01-fundamentals/02-control-flow/03-looping/01-for-loop.js
@@ -1,7 +1,22 @@
 let numbers = [1,2,3,4,5,6,7,8,9];
 
-let index = 0;
-let lengthOfArray = numbers.length;
+// let index = 0;
+// let lengthOfArray = numbers.length;
 
-console.log(numbers[index]);
-// 1
+// console.log(numbers[index]);
+// // 1
+
+for (let index = 0; index <= numbers.length; index++){
+    console.log(index);
+    /* 
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9 */
+}
