3ef27a078eecb1ae61c7e947e51af5c69959a051
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-09 15:45:49 +0700

    make simple function to accept and return the maximum number

---
+++ 01-fundamentals/02-control-flow/exercise-max-two-numbers.js
@@ -0,0 +1,10 @@
+// === A function to accept two numbers and return the maximum number ===
+
+function maxNum(num1,num2){
+    return num1 >= num2 ? num1 : num2
+}
+console.log(maxNum(10,1));
+// Max num: 10
+
+console.log(maxNum(10,100));
+// Max num: 100
