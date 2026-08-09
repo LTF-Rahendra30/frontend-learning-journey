44ea614539c75d7c1c48075f7a4d95dde57c0bf7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-09 16:39:34 +0700

    feat: Implement analyzeNumbers function to evaluate even, odd, max, and min values

--- 01-fundamentals/02-control-flow/project/number-checker.js
+++ 01-fundamentals/02-control-flow/project/number-checker.js
@@ -4,3 +4,28 @@
 - Odd number
 - Max number
 - Min Number */
+
+function analyzeNumbers(numbers){
+    let evenNumber = 0;
+    let oddNumber = 0;
+    let maxNumber = numbers[0];
+    let minNumber = numbers[0];
+
+        // Loop num in array
+    for (let i = 0; i < numbers.length; i++){
+        const num = numbers[i];
+
+        if (num % 2 === 0){
+            evenNumber ++;
+        } else{
+            oddNumber ++;
+        }
+
+        if(num > maxNumber) maxNumber = num;
+        if (num < minNumber) minNumber = num;
+    }
+
+    return {evenNumber, oddNumber,maxNumber,minNumber}
+}
+let result = analyzeNumbers([2, 5, 8, 12, 3, 7]);
+console.log(result);
