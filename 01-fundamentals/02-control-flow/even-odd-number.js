82114ee5beb252fb54421d806f177b97921f3646
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-09 16:13:22 +0700

    feat: Add function display Odd Number

--- 01-fundamentals/02-control-flow/even-odd-number.js
+++ 01-fundamentals/02-control-flow/even-odd-number.js
@@ -1,6 +1,8 @@
 
 //  ====== Even and Odd number with array as Parameter
 
+
+// ===== Even Number ====
 function displayEvenNumber(number){
     for (const num of number){
         if (num % 2 === 0){
@@ -9,5 +11,29 @@ function displayEvenNumber(number){
     }
 }
 
-const array = [1,2,3,4,5,6,7,8,9,10];
-displayEvenNumber(array);
+const arrayEvenNumber = [1,2,3,4,5,6,7,8,9,10];
+displayEvenNumber(arrayEvenNumber);
+/* 2
+4
+6
+8
+10
+*/
+
+
+// ===== Odd Number Function =====
+
+function displayOddNumber(number){
+    for(const num of number){
+        if(num % 2 != 0){
+            console.log(num);            
+        }
+    }
+}
+const arrayOddNumber = [1,2,3,4,5,6,7,8,9,10];
+displayOddNumber(arrayOddNumber);
+/* 1
+3
+5
+7
+9 */
