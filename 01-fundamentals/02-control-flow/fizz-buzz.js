505634c4db62fe5c5cb248bca342477e71d07d92
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-09 15:56:22 +0700

    fix: update fizzBuzz function to correctly handle FizzBuzz condition

--- 01-fundamentals/02-control-flow/fizz-buzz.js
+++ 01-fundamentals/02-control-flow/fizz-buzz.js
@@ -2,12 +2,15 @@
 // Return "Buzz" if only divisible by 5
 
 function fizzBuzz(num){
-    if (num %3 === 0){
+    if (num %3 === 0 && num %5 ===0){
+        return "FizzBuzz"
+    } else if (num %3 === 0){
         return "Fizz";
     } else if(num % 5 === 0){
         return "Buzz"
+    } else{
+        return num;
     }
-    return num;
 }
 
-console.log(fizzBuzz(7));
+console.log(fizzBuzz(16));
