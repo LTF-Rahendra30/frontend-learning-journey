1068a0b818eb79473848800ba86df3cd75397925
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-10 23:00:19 +0700

    feat Correct cart variable name and Add calculateChartPrice function logic

--- 01-fundamentals/02-control-flow/project/cart-calculator.js
+++ 01-fundamentals/02-control-flow/project/cart-calculator.js
@@ -3,11 +3,21 @@ If total >= 100k, get discount 10%
 If total >= 200k, get discount 15% */
 
 // Example Cart
-const cart = [
-  { name: "Keyboard", price: 150000, qty: 1 },
-  { name: "Mouse", price: 50000, qty: 2 },
+const carts = [
+  { name: "Keyboard", price: 1500, qty: 1 },
+  { name: "Mouse", price: 500, qty: 2 },
   { name: "Monitor", price: 0, qty: 1 }  // Invalid, skip
 ];
 
-calculateCart(cart);  
-// Output: { subtotal: 250000, discount: 37500, total: 212500 }
+function calculateChartPrice(cart){
+    let subTotal = 0;
+
+    for (let i = 0; i < carts.lenght; i++){
+        const item = carts[i];
+
+        // validation valid chart item & price
+        if (item.qty >= 1 && item.price > 0){
+            subTotal += item.price * item.qty
+        }
+    }
+}
