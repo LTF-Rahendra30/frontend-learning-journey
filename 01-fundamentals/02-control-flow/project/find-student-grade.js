9fb8896c732bd2c425a9d1d221e67213f4712331
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-10 22:46:37 +0700

    feat: Make simple function, loop in through array, find object and calculate grade

--- 01-fundamentals/02-control-flow/project/find-student-grade.js
+++ 01-fundamentals/02-control-flow/project/find-student-grade.js
@@ -5,3 +5,20 @@ const students = [
   { name: "Dina", score: 65 }
 ];
 
+
+function findStudentGrade(name){
+    for (let i =0; i < students.length; i++){
+
+        // Find Student
+        if(students[i].name.toLocaleLowerCase() === name.toLocaleLowerCase()){
+            // Find Score
+            const score = students[i].score;
+            if(score>=85) return "A";
+            else if(score >= 70) return "B";
+            else if(score >= 60) return "C";
+            else return "Remedial";
+        }
+    }
+
+    return "Not Found";
+}
--- test.js
+++ test.js
@@ -1,6 +1,13 @@
-let data = Number("");
-console.log(data);
+// let data = Number("");
+// console.log(data);
 
-if (!isNaN(data)){
-    console.log("This NaN");
+// if (!isNaN(data)){
+//     console.log("This NaN");
+// }
+
+
+function hello(name){
+    return name.toLowerCase();
 }
+
+console.log(hello("WOWO"));
