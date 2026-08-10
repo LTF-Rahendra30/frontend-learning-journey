003ca079bf85a584b2f83a8478c76680de95ae49
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-10 21:36:40 +0700

    Added comment Rules

---
+++ 01-fundamentals/02-control-flow/project/ticket-price-calc.js
@@ -0,0 +1,8 @@
+// Calculate the cinema ticket price based on age and the type of day (weekday/weekend). Discounts are available for seniors and children, and weekday tickets are cheaper.
+
+/* Base Price : 50k
+Weekend: +25%
+Age < 12 : -30%
+Adult : normal
+Elderly: -20%
+ */
