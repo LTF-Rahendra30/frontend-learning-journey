8a7d1d3c4b0ee713ca1b6d5f8a012b4db48f8723
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-10 21:57:10 +0700

    feat: Make simple function to calculaye ticket price by rules

--- 01-fundamentals/02-control-flow/project/ticket-price-calc.js
+++ 01-fundamentals/02-control-flow/project/ticket-price-calc.js
@@ -6,3 +6,24 @@ Age < 12 : -30%
 Adult : normal
 Elderly: -20%
  */
+
+function calculateTicket(age,isWeekend){
+    let price = 50000;
+
+    // Adjust Weekend
+    if (isWeekend === true){
+        price += (price * 0.25);
+    }
+    if (age < 12){
+        price -= (price * 0.3);
+    }
+    if(age >= 60){
+        price -= (price * 0.2);
+    }
+
+    return price;
+}
+console.log(
+calculateTicket(98,true));
+
+ 
