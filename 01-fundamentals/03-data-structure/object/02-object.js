ab86f8f8bdc54862df1801c4685be3915e475741
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-16 23:51:07 +0700

    Make simple object with function in value property

---
+++ 01-fundamentals/03-data-structure/object/02-object.js
@@ -0,0 +1,11 @@
+const cat = {
+    name: "Kitty",
+    breed: "Persian",
+    age: 3,
+    eat: function() {
+        console.log("Chomp!");
+    },
+    bark() {
+        console.log("Meow");
+    }
+}
