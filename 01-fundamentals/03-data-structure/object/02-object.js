1aa8a22c4183a53c779f8f99db291ce89383b0ab
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-17 00:01:03 +0700

    feat: function to return object value

--- 01-fundamentals/03-data-structure/object/02-object.js
+++ 01-fundamentals/03-data-structure/object/02-object.js
@@ -9,3 +9,16 @@ const cat = {
         console.log("Meow");
     }
 }
+
+function getCat(name,breed,age,){
+    return {
+        name: name,
+        breed: breed,
+        age: age,
+        eat: function() {
+            console.log("Chomp!");
+        },
+        bark() {
+            console.log("Meow");
+    }
+}
