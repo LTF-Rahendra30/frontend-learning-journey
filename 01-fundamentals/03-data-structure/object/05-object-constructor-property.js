7cef9f10bbc30dfbbaa3440ec3a1b7fe28cd8c7e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-22 12:09:11 +0700

    feat: Add examples of JS included objects and object creation

--- 01-fundamentals/03-data-structure/object/05-object-constructor-property.js
+++ 01-fundamentals/03-data-structure/object/05-object-constructor-property.js
@@ -4,3 +4,15 @@ const user = {
 
 console.log(user.constructor);
 // [Function: Object]
+
+// JS Included Object 
+new String(); // 'Bob'
+new Boolean(); // true & false
+new Number(); //1,2,3
+new Array(); // [1,2,3]
+
+// Try Create Object
+
+const obj = {};
+console.log(obj.constructor);
+// [Function: Object]
