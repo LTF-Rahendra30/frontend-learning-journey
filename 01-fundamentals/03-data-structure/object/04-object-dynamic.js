50f3e31a323d825dc591a1050f78525fedadce20
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-22 10:40:51 +0700

    Try Update new property of Object

--- 01-fundamentals/03-data-structure/object/04-object-dynamic.js
+++ 01-fundamentals/03-data-structure/object/04-object-dynamic.js
@@ -2,6 +2,9 @@ const person = {
     name: "Bob"
 }
 
+// Update Property Object
 person.favoriteFood = "Noodels";
+person.isMerried = false;
 console.log(person);
-// { name: 'Bob', favoriteFood: 'Noodels' }
+// { name: 'Bob', favoriteFood: 'Noodels', isMerried: false }
+
