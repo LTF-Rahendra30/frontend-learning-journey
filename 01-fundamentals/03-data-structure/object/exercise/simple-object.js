8f0a6289ff3ab93bd49fc7f9ac963f90f8cf286c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-23 23:55:51 +0700

    Todo change prpoerty and Update property of object

--- 01-fundamentals/03-data-structure/object/exercise/simple-object.js
+++ 01-fundamentals/03-data-structure/object/exercise/simple-object.js
@@ -11,4 +11,20 @@ console.log(user.age);
 /* Bob
 12 */
 
+// ==== Change Property Object ====
+user.hobby = 'Drawing';
+console.log(user);
+// { name: 'Bob', age: 12, isStudent: true, hobby: 'Drawing' }
 
+// ==== Update Property Object ====
+user.city = 'Tokyo';
+console.log(user);
+/* 
+{
+  name: 'Bob',
+  age: 12,
+  isStudent: true,
+  hobby: 'Drawing',
+  city: 'Tokyo'
+}
+ */
