b142086feced779f15fe4b5b872931d44446e0cf
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-23 23:51:52 +0700

    feat: Make folder to start exercise, And make simple Object and display their Porperty

---
+++ 01-fundamentals/03-data-structure/object/exercise/simple-object.js
@@ -0,0 +1,14 @@
+const user = {
+    name: "Bob",
+    age: 12,
+    isStudent: true,
+    hobby: 'Coding'
+}
+
+// ==== Display Property ====
+console.log(user.name);
+console.log(user.age);
+/* Bob
+12 */
+
+
