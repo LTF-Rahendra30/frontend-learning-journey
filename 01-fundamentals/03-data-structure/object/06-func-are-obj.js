4b739b9afd109a9379d9e44dcfb962b0a97367e7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-23 22:15:50 +0700

    feat: Try make constructor function are object

--- 01-fundamentals/03-data-structure/object/06-func-are-obj.js
+++ 01-fundamentals/03-data-structure/object/06-func-are-obj.js
@@ -12,3 +12,42 @@ console.log(n (2,2));
 // Try display parameter in function
 console.log(add.length);
 // 2 parameters
+
+
+// ==== Function Constructor ====
+function Programmer(name){
+    this.name = name;
+
+    // Method
+    this.writeCode = function(){
+        console.log('Write Code Javascript');
+        
+    }
+}
+
+console.log(Programmer.length);
+console.log(Programmer.constructor);
+// 1
+// [Function: Function]
+
+
+// Another Syntax
+
+// Anonymous Function 
+const Person = new Function('name', `
+    this.name = name;
+    this.writeCode = function(){
+        console.log('Write Code Javascript');
+    }
+`);
+
+const newPerson = new Person('Bob');
+console.log(newPerson);
+
+
+console.log(newPerson.name);
+newPerson.writeCode();
+// Bob
+// Write Code Javascript
+
+
