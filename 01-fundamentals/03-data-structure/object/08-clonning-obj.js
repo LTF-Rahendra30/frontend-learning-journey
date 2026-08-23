880f2e307d09482369d96fb4624b63838219c29b
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-23 23:21:19 +0700

    fix: Correct comment and improve code clarity for object cloning example

--- 01-fundamentals/03-data-structure/object/08-clonning-obj.js
+++ 01-fundamentals/03-data-structure/object/08-clonning-obj.js
@@ -1,10 +1,27 @@
 
-// Not Independent variable
+// Not Independent Object
 
-let a = {value: 10};
+/* let a = {value: 10};
 let b = a;
 
 b.value = 20;
 
 console.log(a);
+console.log(b); */
+/* 
+{ value: 20 }
+{ value: 20 } */
+
+
+// Independent Object
+let a = {value: 10};
+let b = {};
+
+Object.assign(b,a);
+
+// Representation 2 Diffrerent Object
+b.value = 20;
+console.log(a);
 console.log(b);
+// { value: 10 }
+// { value: 20 }
