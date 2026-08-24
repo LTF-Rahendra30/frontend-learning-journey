b0217f29deb8e6b7ae3d8efef5b3c764ed15f5cf
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-24 09:54:06 +0700

    feat: Make simple object and try Access object porperty with dot notation

---
+++ 01-fundamentals/03-data-structure/object/exercise/03-acces-manipulation-obj.js
@@ -0,0 +1,16 @@
+
+// ============ Acces and Manipulation Object ==========
+
+const product = {
+    name: "Laptop",
+    price: 10000,
+    stock: 10,
+    rating: 4.8,
+};
+
+// Access property Object
+console.log(product.name);
+console.log(product.price);
+/* Laptop
+10000
+ */
