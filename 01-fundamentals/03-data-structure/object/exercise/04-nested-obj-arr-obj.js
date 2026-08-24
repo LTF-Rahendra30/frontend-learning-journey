cc13cf6cef5705061d9a5d2701c03167a6866825
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-24 10:22:47 +0700

    feat: Make nested Object

--- 01-fundamentals/03-data-structure/object/exercise/04-nested-obj-arr-obj.js
+++ 01-fundamentals/03-data-structure/object/exercise/04-nested-obj-arr-obj.js
@@ -1,2 +1,17 @@
 // =========== NESTED OBJECT & ARRAY OF OBJECTS =======
 
+// --- E-commerce Order Object ----
+
+const order ={
+    id: "ORD-001",
+    customer: {
+        name: "Bob",
+        email: "bob123@gmail.com",
+        address: "Distric Nagoya"
+    },
+    items: [
+        {   id:1, name: "Laptop", price: 1000, qty: 2   },
+        {   id:1, name: "Keyboard", price: 500, qty: 2   },
+        {   id:1, name: "Mouse", price: 200, qty: 1   }
+    ]
+}
