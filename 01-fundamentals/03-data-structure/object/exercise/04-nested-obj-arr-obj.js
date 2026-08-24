e001ae97ecec983d22464a74a9d51a8914dd1a59
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-24 11:19:07 +0700

    feat: calculate Total price Item in order Object after change Before

--- 01-fundamentals/03-data-structure/object/exercise/04-nested-obj-arr-obj.js
+++ 01-fundamentals/03-data-structure/object/exercise/04-nested-obj-arr-obj.js
@@ -47,3 +47,31 @@ console.log(order.customer);
 order.items[2].qty = 5;
 console.log(order.items[2]);
 // { id: 1, name: 'Mouse', price: 200, qty: 5 }
+
+
+// ---------- Over all object value now, after change before ----------
+console.log(order);
+// {
+//   id: 'ORD-001',
+//   customer: { name: 'Bob', email: 'bob123@gmail.com', address: 'Tokyo' },
+//   items: [
+//     { id: 1, name: 'Laptop', price: 1000, qty: 2 },
+//     { id: 1, name: 'Keyboard', price: 500, qty: 2 },
+//     { id: 1, name: 'Mouse', price: 200, qty: 5 }
+//   ]
+// }
+
+
+// ========== Calculate Total Price ===========
+let subTotal = 0;
+for(let i = 0; i < order.items.length; i++){
+    let item = order.items[i];
+
+    // caluculate price with quantity
+    let subTotalItems = item.price * item.qty;
+
+    // calculate item in object
+    subTotal += subTotalItems;
+};
+console.log(subTotal);
+// 4000
