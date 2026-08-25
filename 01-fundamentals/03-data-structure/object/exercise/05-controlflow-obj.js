372a208ec0f8710ac69515c225607aabf4533120
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-25 12:42:41 +0700

    TODO B: Calculate all of value Inventory

--- 01-fundamentals/03-data-structure/object/exercise/05-controlflow-obj.js
+++ 01-fundamentals/03-data-structure/object/exercise/05-controlflow-obj.js
@@ -20,3 +20,15 @@ for (let i = 0; i < products.length; i++){
     );
     
 }
+
+
+// TODO B: Calculate all of value Inventory
+let totalInventoryValue = 0;
+for (let i = 0; i < products.length; i++){
+    let product = products[i];
+    let productvalue = product.price * product.stock;
+
+    totalInventoryValue += productvalue
+}
+console.log(totalInventoryValue);
+// 18500
