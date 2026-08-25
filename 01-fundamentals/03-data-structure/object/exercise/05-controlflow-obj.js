c633043a875c4edeee67fe60894600cf3039deb4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-25 12:26:49 +0700

    feat: Create simple object for example

--- 01-fundamentals/03-data-structure/object/exercise/05-controlflow-obj.js
+++ 01-fundamentals/03-data-structure/object/exercise/05-controlflow-obj.js
@@ -1,2 +1,12 @@
 // =============== LOOP + CONDITIONAL + OBJECT LOGIC ==============
 
+// ============= SIMPLE INVENTORY MANAGEMENT ==============
+
+
+const products = [
+    { id: 1, name: "Laptop", price: 1500, stock: 3, category: "Electronics" },
+    { id: 2, name: "Book", price: 100, stock: 10, category: "Hoby" },
+    { id: 3, name: "Keyboard", price: 1000, stock: 5, category: "Electronics"},
+    { id: 4, name: "Shoes", price: 200, stock: 15, category: "Fashion"},
+    { id: 5, name: "Jacket", price: 500, stock: 10, category: "Fashion"},
+];
