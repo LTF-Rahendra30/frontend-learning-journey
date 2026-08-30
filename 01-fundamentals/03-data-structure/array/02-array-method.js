8d01e4fda8912365ba7099d20aad2f87a8c5703d
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-30 14:56:18 +0700

    feat: Add examples for unshift and splice array methods

--- 01-fundamentals/03-data-structure/array/02-array-method.js
+++ 01-fundamentals/03-data-structure/array/02-array-method.js
@@ -5,3 +5,22 @@ let number = [1,2,3,4,5];
 number.push(6,7,8);
 console.log(number);
 // [ 1, 2, 3, 4, 5, 6,, 7, 8]
+
+
+// --------- Unshift --------
+// Added element in the beginning of array
+number.unshift("Beginning array");
+console.log(number);
+// [ 'Beginning array', 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+
+
+// ----------- SPLICE ---------
+// delete,change and added new element in array
+const buah = ["Apel", "Pisang", "Jeruk", "Mangga"];
+console.log(buah);
+// [ 'Apel', 'Pisang', 'Jeruk', 'Mangga' ]
+
+buah.splice(1,1); // delete form index 1, amount index that delete is 1
+console.log(buah);
+// [ 'Apel', 'Jeruk', 'Mangga' ]
