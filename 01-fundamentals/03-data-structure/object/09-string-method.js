6f932b959b3d450f06ef92802443fbf4a271d216
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-30 14:03:25 +0700

    feat: Try Include and Startwith method of string

--- 01-fundamentals/03-data-structure/object/09-string-method.js
+++ 01-fundamentals/03-data-structure/object/09-string-method.js
@@ -1,3 +1,17 @@
+let name = "bob";
+console.log(typeof name); // string
+
+let anotherName = new String('Chloe');
+console.log(typeof anotherName); // Object
+
+
+// ----- Include Method ----
+
 let word = "The desentralized";
 const included = word.includes("The")
 console.log(included); // True
+
+
+// ----- Start with method ------
+const startWith = word.startsWith('T');
+console.log(startWith); // true
