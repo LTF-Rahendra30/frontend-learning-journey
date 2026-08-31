d9cd6ff517f03c2cd7bea729da7800a3353e8a77
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-31 15:38:41 +0700

    Try usage find() method more complate for serch element in array (array  object) with criteria

--- 01-fundamentals/03-data-structure/array/04-find-element-referance.js
+++ 01-fundamentals/03-data-structure/array/04-find-element-referance.js
@@ -1,14 +1,40 @@
 
 // =========== FINDING ELEMENT REFERANCE =====
 
-const users = [
+const peapole = [
     {name : "Bob", age: 12},
     {name : "Chloe", age: 18},
     {name : "Charley", age: 15}
 ];
 
-const person = users.find(function(e){
-    return e.name === "Bob";
+const person = peapole.find((element) => {
+    return element.name === "Bob";
 });
 
 console.log(person);
+
+
+// ---- Usage Find Method for find number > 20 ----
+const num = [2,4,6,7,9,12,14,17,18,19,22,25,27];
+const result = num.find(n => n > 20);
+console.log(result); // 22
+
+// -------- Usage Find method to search uniqie id user ------
+
+const users = [
+  { id: 101, nama: 'Andi', peran: 'User' },
+  { id: 102, nama: 'Budi', peran: 'Admin' },
+  { id: 103, nama: 'Cici', peran: 'User' }
+];
+
+function findUserById(id){
+    const result = users.find(usr => usr.id === id);
+    return console.log(result);   
+}
+
+findUserById(101);
+findUserById(102);
+/* 
+{ id: 101, nama: 'Andi', peran: 'User' }
+{ id: 102, nama: 'Budi', peran: 'Admin' }
+  */
