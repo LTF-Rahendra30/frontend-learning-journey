b36390050617358cf7101c9916f029ba1ea176bf
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-31 14:56:55 +0700

    Try: Finding index but the element is unknown

--- 01-fundamentals/03-data-structure/array/03-finding-element.js
+++ 01-fundamentals/03-data-structure/array/03-finding-element.js
@@ -13,3 +13,18 @@ const person = ["Andrew","Bob","Chloe", "Dory","Chloe"];
 const posisition = person.lastIndexOf("Chloe");
 console.log(posisition); // 4
 
+// ---- Finding index but the element is unknown ----
+
+const num = [1,2,3,4,5];
+const indexOfSix = num.indexOf(6);
+// console.log(indexOfSix); // -1
+
+// --- Solve ---
+if (indexOfSix !== -1){
+    console.log(indexOfSix);
+
+} else{
+    console.log("6 is not found in the array");
+    
+}
+ 
