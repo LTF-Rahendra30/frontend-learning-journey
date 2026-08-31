803ad8d2daf51d9763f1c768ff1c172ff14c7567
LTF-Rahendra30 <kecuu91@gmail.com>
2026-08-31 14:52:09 +0700

    Try Finding last index of array element

--- 01-fundamentals/03-data-structure/array/03-finding-element.js
+++ 01-fundamentals/03-data-structure/array/03-finding-element.js
@@ -4,4 +4,12 @@ const number = [1,2,3,4,5];
 
 // ---- Finding Index in element ----
 const indexOfThree = number.indexOf(3);
-console.log(indexOfThree);
+console.log(indexOfThree);  //2
+
+
+// ------ Finding Last index ---------
+const person = ["Andrew","Bob","Chloe", "Dory","Chloe"];
+
+const posisition = person.lastIndexOf("Chloe");
+console.log(posisition); // 4
+
