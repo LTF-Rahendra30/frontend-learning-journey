c6be1a9582eb253276357ebc34f006c804bc0597
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 20:03:58 +0700

    feat: Create new folder and new file to start try Modern Method of Array

---
+++ 02-intermediate/01-array-method/01-map-method.js
@@ -0,0 +1,10 @@
+// === MAP() , CHANGE ELEMENT TO NEW REPERENTATION ======
+
+const num = [1,2,3,4];
+const doubled = num.map(n => n * 2);
+
+console.log(num);
+// [ 1, 2, 3, 4 ]
+
+console.log(doubled);
+// [ 2, 4, 6, 8 ]
