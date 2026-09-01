dc34f95800fe38b16857743ce9dd206e96a2e791
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 14:42:22 +0700

    feat: Try delete first element and try delete element with Splice() Method

--- 01-fundamentals/03-data-structure/array/06-removing-element.js
+++ 01-fundamentals/03-data-structure/array/06-removing-element.js
@@ -7,3 +7,30 @@ console.log(`Last element: ${lastElement}`);
 
 // [ 1, 2, 3, 4 ]
 // Last element: 5
+
+
+// ==== FIRST ELEMENT =====
+const firstElement = number.shift();
+console.log(number);
+console.log(`First element: ${firstElement}`);
+/* 
+[ 2, 3, 4 ]
+First element: 1
+ */
+
+
+// ======= MID ELEMENT With Splice() =====
+const buah = ["Apel", "Pisang", "Jeruk", "Mangga"];
+buah.splice(0,1); // Delete from index 0, amount delete 1
+console.log(buah);
+// [ 'Pisang', 'Jeruk', 'Mangga' ]
+
+// ---- Delete 2, from index 0 ----
+const alfabet = ['A','B','C','D','E'];
+alfabet.splice(0,2);
+console.log(alfabet);
+
+// ------ Delete 0 from index 4, and add element ----
+alfabet.splice(4,0, 'F','G');
+console.log(alfabet);
+// [ 'C', 'D', 'E', 'F', 'G' ]
