7680f9a2f81f504d1264d0db08492665ac727b1c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 14:57:22 +0700

    feat: Make new file to start Try spread operators

---
+++ 01-fundamentals/03-data-structure/array/08-spread-operators.js
@@ -0,0 +1,14 @@
+// SCPEAD OPERATPORS
+
+const arr1 = [1,2,3,4];
+const arr2 = [5,6,7,8];
+
+
+const combine = [...arr1, ...arr2];
+console.log(combine);
+/* 
+[
+  1, 2, 3, 4,
+  5, 6, 7, 8
+]
+   */
