4d33f3096e5924b7998b8ab3360815b8cf26daec
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 15:26:03 +0700

    feat: Update forEach example and add traditional indexing demonstration

--- 01-fundamentals/03-data-structure/array/09-for-each.js
+++ 01-fundamentals/03-data-structure/array/09-for-each.js
@@ -1,7 +1,7 @@
 const number = [1,2,3,4,5,6,7,8];
 
 // Traditional loop
-for (let num of number) console.log(num);
+// for (let num of number) console.log(num);
 
 
 // =========== FOR EACH () METHOD =========
@@ -17,3 +17,21 @@ number.forEach((num) => {
     7
     8 */
 })
+
+// ------ Indexing ----------
+number.forEach((num,index) => {
+    console.log(`Index: ${index}, Number: ${num}`);
+    /* Index: 0, Number: 1
+        Index: 1, Number: 2
+        Index: 2, Number: 3
+        Index: 3, Number: 4
+        Index: 4, Number: 5
+        Index: 5, Number: 6
+        Index: 6, Number: 7
+        Index: 7, Number: 8 */
+});
+
+// ---------- traditional indexing ----------
+for (let i = 0; i < number.length; i ++){
+    console.log(`Index: ${i}, Number: ${number[i]}`);
+}
