c44d8a255979d3b2aa17d8223ef47d19fd95c9ef
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 20:19:12 +0700

    feat: Add exemple array of object to filtering by conditional

--- 02-intermediate/01-array-method/02-filter-method.js
+++ 02-intermediate/01-array-method/02-filter-method.js
@@ -9,3 +9,21 @@ console.log(evenNumber);
 const oddNumber = number.filter(num => num %2 !== 0);
 console.log(oddNumber);
 // [ 1, 3, 5, 7, 9 ]
+
+
+// ===== Example ====
+const products = [
+    { id: 1, name: "Laptop", price: 1500, stock: 0, category: "Electronics" },
+    { id: 2, name: "Book", price: 100, stock: 10, category: "Hoby" },
+    { id: 3, name: "Keyboard", price: 1000, stock: 0, category: "Electronics"},
+    { id: 4, name: "Shoes", price: 200, stock: 15, category: "Fashion"},
+    { id: 5, name: "Jacket", price: 500, stock: 0, category: "Fashion"}
+];
+
+// ---- Filtering stock product > 0 ----
+const validStockProduct = products.filter(product => {
+   return product.stock > 0;
+})
+
+console.log(products);
+console.log(validStockProduct);
