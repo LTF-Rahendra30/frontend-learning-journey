3fd45209b840aad4827b899a8132cc36e17cd211
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 15:10:29 +0700

    Try change array without change original with Spread Operators

--- 01-fundamentals/03-data-structure/array/08-spread-operators.js
+++ 01-fundamentals/03-data-structure/array/08-spread-operators.js
@@ -12,3 +12,22 @@ console.log(combine);
   5, 6, 7, 8
 ]
    */
+
+const combine2 = [...arr1, "hello",...arr2 , "Welcome"];
+console.log(combine2);
+
+// [ 1, 2, 3, 4, 'hello', 5, 6, 7, 8, 'Welcome' ]
+
+
+
+
+// ======= CLONE OLD ARRAY WITHOUT CHANGE ORIGINAL ARRAY ===========
+
+const originalUser = ['Andrew','Bob','Chloe'];
+console.log(originalUser);
+// [ 'Andrew', 'Bob', 'Chloe' ]
+
+
+const newUser = [...originalUser, 'Duts'];
+console.log(newUser);
+// [ 'Andrew', 'Bob', 'Chloe', 'Duts' ]
