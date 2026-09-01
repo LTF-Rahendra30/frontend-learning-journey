81a9561a1679bdb2679cdb3c40413f3f15ba50ea
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-01 14:32:39 +0700

    feat: Make new file to Try remove element method and try pop() method

---
+++ 01-fundamentals/03-data-structure/array/06-removing-element.js
@@ -0,0 +1,9 @@
+const number = [1,2,3,4,5];
+
+// === POP , Delete from last element ===
+const lastElement = number.pop();
+console.log(number);
+console.log(`Last element: ${lastElement}`);
+
+// [ 1, 2, 3, 4 ]
+// Last element: 5
