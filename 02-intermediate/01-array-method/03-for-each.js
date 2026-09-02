b6867db6eaf40596747392a2d9e18b670a8b102c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-02 22:36:55 +0700

    feat: Try usage of Map() method for array

--- 02-intermediate/01-array-method/03-for-each.js
+++ 02-intermediate/01-array-method/03-for-each.js
@@ -41,3 +41,24 @@ carts.forEach(item => {
     
 })
 console.log(`Final Payment: ${finalPayment}`);
+
+// ------- MAP() METHOD --------
+/* Skenario: Frontend developer membutuhkan data baru dari data di atas untuk ditampilkan ke UI, di mana setiap objek harus memiliki properti baru bernama subTotal.
+ */
+
+
+const dataForUI = carts.map(item => {
+    return {
+        productName: item.name,
+        prodctPrice: item.price,
+        subTotal: item.stock * item.price
+    }
+    /* 
+     [
+  { productName: 'Laptop', prodctPrice: 1500, subTotal: 3000 },
+  { productName: 'Book', prodctPrice: 100, subTotal: 1000 },
+  { productName: 'Keyboard', prodctPrice: 1000, subTotal: 5000 }
+] */
+})
+
+console.log("\n",dataForUI);
