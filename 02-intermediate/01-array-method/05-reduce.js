1d44d376d79d6be103b7af85c495843aca7edba4
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-02 23:29:37 +0700

    feat: Add use case for reduce() method with transaction filtering

--- 02-intermediate/01-array-method/05-reduce.js
+++ 02-intermediate/01-array-method/05-reduce.js
@@ -3,3 +3,19 @@ const sum = number.reduce((accumulator,current) => {
     return accumulator + current
 },0);
 console.log(sum);
+
+
+// --- Use Case ----
+
+const riwayatTransaksi = [
+  { produk: "Kemeja Flanel", total: 200000, status: "BERHASIL" },
+  { produk: "Celana Chino", total: 350000, status: "GAGAL" },
+  { produk: "Jaket Bomber", total: 450000, status: "BERHASIL" }
+];
+
+const amountValidTransaction = riwayatTransaksi.filter(transaction => transaction.status === "BERHASIL")
+    .reduce((accumulator,transactionNow) => {
+        return accumulator + transactionNow.total;
+    },0); // <-- 0 adalah nilaiAwal (kalkulasi dimulai dari angka 0)
+
+console.log(`Valid Transaction: ${amountValidTransaction}`);
