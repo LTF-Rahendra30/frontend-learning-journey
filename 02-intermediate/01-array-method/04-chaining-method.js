fc733177dee6c56a6f6716f48be930acb301b037
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-02 23:10:39 +0700

    feat: try another example that using map,forEach and filter method of array

--- 02-intermediate/01-array-method/04-chaining-method.js
+++ 02-intermediate/01-array-method/04-chaining-method.js
@@ -18,3 +18,29 @@ console.log(validTransaction);
   '[TX01] Kemeja Flanel | Rp 200.000',
   '[TX02] Celana Chino | Rp 350.000'
 ] */
+
+
+// -------- CHINING FILTER,MAP AND FOR-EACH() ========
+const transactionHistory2 = [
+  { id: "TX01", produk: "Kemeja Flanel", total: 200000, status: true, hp: "08123" },
+  { id: "TX02", produk: "Celana Chino", total: 350000, statuts: false, hp: "08456" },
+  { id: "TX03", produk: "Jaket Bomber", total: 450000, status: true, hp: "08789" }
+];
+
+function sendAPI(no,massage){
+    console.log(`[System] Send to ${no}: ${massage}`);
+    
+}
+
+transactionHistory2
+    .filter(transaction => transaction.status === true)
+    .map(transaction => {
+        return {
+            noHp : transaction.hp,
+            massage: `Hello, order ${transaction.produk} | Value: ${transaction.total}`
+        };
+    })
+    .forEach(notifData => {
+        sendAPI(notifData.noHp,notifData.massage);
+    });
+
