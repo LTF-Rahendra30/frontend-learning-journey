b1f40bc81ac18fd2ff95e0da998c8acbaae11df7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-02 22:19:02 +0700

    Try access index element use forEach()

--- 02-intermediate/01-array-method/03-for-each.js
+++ 02-intermediate/01-array-method/03-for-each.js
@@ -1,3 +1,5 @@
+
+// ===== USAGE  FOR LOOPING EVERY ELEMENT IN THE ARRAY ==========
 const names  = ['Adi', 'Budi', 'Citra'];
 names.forEach(name => {
     console.log(`Hello ${name}`);
@@ -5,4 +7,17 @@ names.forEach(name => {
     Hello Adi
     Hello Budi
     Hello Citra */
+});
+
+
+
+// ===== ACCESS INDEX ELEMENT =====
+const color = ['red','green','blue'];
+
+color.forEach((item,index) => {
+    console.log(`Index to- ${index} : ${item}`);
+    /* 
+    Index to- 0 : red
+    Index to- 1 : green
+    Index to- 2 : blue */
 })
