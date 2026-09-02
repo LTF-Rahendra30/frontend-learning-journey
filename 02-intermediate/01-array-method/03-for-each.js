b553b162ebd85e2b9f2638fb564b15473bd5f215
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-02 22:29:56 +0700

    feat: Added usage of forEach() method of Array

--- 02-intermediate/01-array-method/03-for-each.js
+++ 02-intermediate/01-array-method/03-for-each.js
@@ -20,4 +20,24 @@ color.forEach((item,index) => {
     Index to- 0 : red
     Index to- 1 : green
     Index to- 2 : blue */
+});
+
+
+// ========== EXAMPLE USECASE BETWEEN FOREACH() AND MAP() =========
+
+const carts = [
+    { id: 1, name: "Laptop", price: 1500, stock: 2, category: "Electronics" },
+    { id: 2, name: "Book", price: 100, stock: 10, category: "Hoby" },
+    { id: 3, name: "Keyboard", price: 1000, stock: 5, category: "Electronics"}
+];
+
+let finalPayment = 0;
+
+carts.forEach(item => {
+    const subTotal = item.stock * item.price;
+
+    finalPayment += subTotal;
+    console.log(`Procduct: ${item.name}, Amount: ${item.stock}, Price: ${item.price}`);
+    
 })
+console.log(`Final Payment: ${finalPayment}`);
