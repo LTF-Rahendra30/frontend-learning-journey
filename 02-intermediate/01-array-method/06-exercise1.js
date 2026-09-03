f1b8a43582a8b97397833b9c0d12ac34fa53f0b8
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-03 16:21:33 +0700

    feat: Added new file to start new exercise

---
+++ 02-intermediate/01-array-method/06-exercise1.js
@@ -0,0 +1,12 @@
+const transactions = [
+  { id: 1, amount: 50000, status: 'success' },
+  { id: 2, amount: 30000, status: 'pending' },
+  { id: 3, amount: 75000, status: 'success' },
+  { id: 4, amount: 20000, status: 'failed' }
+];
+
+/* 
+1. take transaction that success
+2. Make new array
+3. calculate all amount
+*/
