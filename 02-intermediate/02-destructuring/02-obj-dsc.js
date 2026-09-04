4cc4267389de9ab9abe403f6dd4a6362890da126
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-04 11:31:55 +0700

    feat: Add new filre to start try Object destructuring

---
+++ 02-intermediate/02-destructuring/02-obj-dsc.js
@@ -0,0 +1,11 @@
+const user = { name: "Alice", age: 25, country: "USA" };
+
+const {name, country,age} = user;
+console.log(name);
+console.log(country);
+console.log(age);
+
+/* Alice
+USA
+25
+ */
