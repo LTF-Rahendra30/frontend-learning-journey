b982175769a9683856b86ff332eaaba5663f5489
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-04 11:39:14 +0700

    try spread operator combine with destructuring in Array

--- 02-intermediate/02-destructuring/01-array-dsc.js
+++ 02-intermediate/02-destructuring/01-array-dsc.js
@@ -6,6 +6,12 @@ console.log(n);
 
 
 // Destructuring Array
-const [a,b,c] = number;
-console.log(a,b,c);
+// const [a,b,c] = number;
+// console.log(a,b,c);
 // 1 2 3
+
+// Spread operator
+
+const [a,b,c, ...rest] = number;
+console.log(rest);
+// [ '4', '5' ]
