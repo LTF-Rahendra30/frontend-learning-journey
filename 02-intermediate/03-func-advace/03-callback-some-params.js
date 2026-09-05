94d61464937a10c7a895ffcc4bc1d14f1e08ffa8
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-05 12:17:34 +0700

    feat: Try callback with named function

--- 02-intermediate/03-func-advace/03-callback-some-params.js
+++ 02-intermediate/03-func-advace/03-callback-some-params.js
@@ -9,3 +9,21 @@ calculate(90,10, (resultCalc, numA, numB) => {
     console.log(`${numA} + ${numB},Result is: ${resultCalc}`);
     
 })
+
+
+// =========== CALLBACK WITH NAMED FUNCTION ===========
+
+function ending(){
+    console.log("Proccess end");
+}
+
+function startProccess(callback){
+    console.log("Proccess start");
+    callback()
+}
+
+// ---------- Usage --------
+startProccess(ending);
+/* 
+Proccess start
+Proccess end */
