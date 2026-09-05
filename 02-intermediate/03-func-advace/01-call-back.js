3dd630ceddfe3274c4221d9e67fe16383acca0db
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-05 11:26:24 +0700

    feat: Add callback function example and usage in 01-call-back.js

--- 02-intermediate/03-func-advace/01-call-back.js
+++ 02-intermediate/03-func-advace/01-call-back.js
@@ -6,3 +6,19 @@ run(() => {
     console.log("Call Back Running");
     
 })
+
+
+// ====== CALBACK =====
+
+// MAIN FUNCTION
+function jalankan(callback){
+    callback();
+}
+
+// FUNCTION AS A ARGUMENT IN MAIN FUNCTION
+function Hello(){
+    console.log("Hello");
+}
+
+// USAGE 
+jalankan(Hello)
