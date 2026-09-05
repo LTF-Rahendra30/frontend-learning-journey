e570ddb4939b8708dbb6a211abdb9d2fec43d9e7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-05 12:13:54 +0700

    feat: Add calculate function with callback to demonstrate parameter usage

--- 02-intermediate/03-func-advace/03-callback-some-params.js
+++ 02-intermediate/03-func-advace/03-callback-some-params.js
@@ -1,1 +1,11 @@
-// 
+// ======= CALCULATE TWO FUNCTION ======
+
+function calculate(a,b,callback){
+    const result = a + b;
+    callback(result,a,b);
+}
+
+calculate(90,10, (resultCalc, numA, numB) => {
+    console.log(`${numA} + ${numB},Result is: ${resultCalc}`);
+    
+})
