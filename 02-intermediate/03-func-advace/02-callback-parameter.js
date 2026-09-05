e53cb48492454c7ba2afc54854c439c767ed0e30
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-05 12:01:54 +0700

    Try callback params with arraow function

--- 02-intermediate/03-func-advace/02-callback-parameter.js
+++ 02-intermediate/03-func-advace/02-callback-parameter.js
@@ -9,3 +9,19 @@ prosesNama("bob", function(resultName){
     console.log(`Name: ${resultName}`);
 });
 // Name: BOB
+
+// ---------- arrow function ---------
+function multiply(agrs,callback){
+    const result = agrs ** 2;
+    callback(result);
+    /* ==== >  console.log(`result: ${resulr}`);
+    
+    */
+
+}
+
+// ------ Usage ----
+multiply(10, resultMultiply => {
+    console.log(`result: ${resultMultiply}`);
+    // result: 100
+})
