6580313a91152d572b93266c7e6c9417dcb0d55f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-05 12:24:01 +0700

    try Callback with Set time out

---
+++ 02-intermediate/03-func-advace/04-settimeout.js
@@ -0,0 +1,10 @@
+// ============ CALLBACK WITH SET TIMEOUT =============
+
+
+console.log("First");
+
+setTimeout(() => {
+    console.log("Callback running after 2 second");
+    
+}, 2000);
+console.log("Second");
