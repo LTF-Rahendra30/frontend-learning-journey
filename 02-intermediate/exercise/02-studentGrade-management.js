61217fdeb18d119559bd07d707e1137ed5357c29
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-07 23:57:39 +0700

    fix: Correct parameter name in addGrade() function and update usage

--- 02-intermediate/exercise/02-studentGrade-management.js
+++ 02-intermediate/exercise/02-studentGrade-management.js
@@ -21,22 +21,28 @@ const classroom = {
         classroom.students.push(newStudents)
     },
 
-    addGrade : (studenId,grade) => {
+    addGrade : (studentId,grade) => {
         if (typeof grade !== 'number' || grade < 0 || grade > 100){
             return "Grade must be number and range 0-100";
         }
         // Found student
-        const student = classroom.students.find(s => s.id === studenId);
+        const student = classroom.students.find(s => s.id === studentId);
 
         // Push Grade
         if (student){
             student.grades.push(grade);
+            console.log(`Success added grade: ${grade} in ${student.name}`);
+            
+        } else{
+            console.log(`Not found or user by ID : ${studentId}`);
+            
         }
     }
 }
 classroom.addStudent("Bob");
 classroom.addStudent("Chloe");
-classroom.addGrade("Bob",33);
+classroom.addGrade(1,33);
+classroom.addGrade(1,33);
 console.log(classroom.students);
 
 
