adbf1f32e75ceb814931184c5138737a30560344
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-07 23:01:02 +0700

    Make structure database and make Function property for usage

---
+++ 02-intermediate/exercise/02-studentGrade-management.js
@@ -0,0 +1,16 @@
+/* EXERCISE: Student Grade Management System
+RULSES:
+1. Add stusent
+2. Inpute grade
+3. Calculate statistic
+4. Return clean Interface
+*/
+
+
+const classroom = {
+    students: [],
+    addSomething : (name) => classroom.students.push("Bob")
+}
+
+classroom.addSomething("Bob");
+console.log(classroom.students);
