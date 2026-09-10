68cd43f44ec014bb04e70d115612062785a8e6b6
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-10 12:58:36 +0700

    feat: Add togle active with simple dom JS

--- project/01-landingpage-restorant/script.js
+++ project/01-landingpage-restorant/script.js
@@ -1,2 +1,6 @@
 const hamburger = document.getElementById('hamburger-btn');
 const navLinks = document.getElementById('nav-links-container');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+})
