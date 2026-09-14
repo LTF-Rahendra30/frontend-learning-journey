7a40e6ad563394cbc8c8d6ed705bfdc719537dc0
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-14 23:55:32 +0700

    Try local variable in the function

---
+++ 01-fundamentals/04-function/04-scope-var.js
@@ -0,0 +1,16 @@
+
+
+// ---- Local var ----
+function func1(){
+    let x = 1;
+    console.log(x);
+    
+}
+function func2(){
+    let x = 2;
+    console.log(x);
+    
+}
+
+func1() // 1
+func2() // 2
