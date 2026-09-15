e1e8e5f248856b565e5d104b252472e18068b143
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-15 19:57:24 +0700

    Try simple example of Function Expresion

---
+++ 01-fundamentals/04-function/07-exxpresion-func.js
@@ -0,0 +1,8 @@
+// ==== FUNCTION THAT STORED IN THE VARIABLE OR AS A VALUE OF VARIABLE ====
+
+const showMssg = function(name){
+    console.log(`Hello ${name}`)
+}
+
+showMssg("Bob");
+// hello bob
