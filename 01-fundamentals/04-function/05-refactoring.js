de936f10809112363851daef3c9d58482a1f538d
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-15 13:30:30 +0700

    feat: Add simple example extraction funct

--- 01-fundamentals/04-function/05-refactoring.js
+++ 01-fundamentals/04-function/05-refactoring.js
@@ -4,3 +4,19 @@ function twoCubeVolume(a,b) {
 
 console.log(twoCubeVolume(10,10));
 // 2000
+
+
+// ============= Extraction Function ============
+const keranjangBelanja = [
+  { produk: "Sepatu Lari", harga: 500000, jumlah: 1 },
+  { produk: "Kaos Polos", harga: 150000, jumlah: 2 },
+  { produk: "Kaos Kaki", harga: 50000, jumlah: 3 }
+];
+
+const dataPengiriman = { kota: "Jakarta", beratKg: 2 };
+const diskonMember = 0.10; // Diskon 10%
+
+// ==== EXTRACTION CALCULATE TOTAL PRICE =====
+function calculateSubtotal(items){
+    return items.reduce((total,item) => total + (item.harga * item.jumlah),0);
+}
