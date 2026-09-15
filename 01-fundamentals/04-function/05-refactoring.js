8a8d603c4a41fca01650cacfa4f87eb66f769695
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-15 13:47:56 +0700

    feat: Add display function and Main function

--- 01-fundamentals/04-function/05-refactoring.js
+++ 01-fundamentals/04-function/05-refactoring.js
@@ -16,6 +16,7 @@ const keranjangBelanja = [
 const dataPengiriman = { kota: "Jakarta", beratKg: 2 };
 const diskonMember = 0.10; // Diskon 10%
 
+
 // ==== EXTRACTION CALCULATE TOTAL PRICE =====
 function calculateSubtotal(items){
     return items.reduce((total,item) => total + (item.harga * item.jumlah),0);
@@ -25,5 +26,24 @@ function calculateSubtotal(items){
 function calculateOngkir(city,weight){
     const ONGKIR_JAKARTA = 10000;
     const TARIF_LUAR = 25000;
-    return city === "Jakarta" ? weight * ONGKIR_JAKARTA : beratKg * TARIF_LUAR;
+    return city === "Jakarta" ? weight * ONGKIR_JAKARTA : weight * TARIF_LUAR;
+}
+// ==== DISPLAY FUNCTION ===
+function displayResult(subTotal,discount,ongkir,finalTotal){
+    console.log(`--- Ringkasan Pembayaran (Clean) ---`);
+    console.log(`Subtotal    : Rp ${subTotal}`);
+    console.log(`Potongan    : Rp ${discount}`);
+    console.log(`Ongkir      : Rp ${ongkir}`);
+    console.log(`Total Bayar : Rp ${finalTotal}`);
+}
+
+// === MAIN FUNCTION ===
+function proccessTransaction(items,pengiriman,discount){
+    const subTotal = calculateSubtotal(items);
+    const ongkir = calculateOngkir(pengiriman.kota,pengiriman.beratKg);
+
+    const discountCut = subTotal * discount;
+    const finalTotal = subTotal - discountCut + ongkir;
+
+    displayResult(subTotal,discountCut,ongkir,finalTotal);
 }
