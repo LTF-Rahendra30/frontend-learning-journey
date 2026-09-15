b978a0a97e2efbe1a8b53156073e28d34252ffda
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-15 23:53:15 +0700

    Try for each with index and Value

--- 02-intermediate/01-array-method/07-forEach2.js
+++ 02-intermediate/01-array-method/07-forEach2.js
@@ -2,7 +2,7 @@ const num = [1,2,3,4,5];
 
 
 // === As a Call back ==
-let callback = function(n){
+function callback(n){
     console.log(n);
 }
 num.forEach(callback);
@@ -12,3 +12,14 @@ num.forEach(callback);
 3
 4
 5 */
+
+// ======= With index and Value =====
+const user = ['andrew','bob','chloe','david'];
+
+user.forEach((usr,idx) => console.log(`User: ${usr} Index: ${idx}`)
+)
+/* User: andrew Index: 0
+User: bob Index: 1
+User: chloe Index: 2
+User: david Index: 3
+ */
