916459cf49aa5891bc1dd979c879305cc0554c00
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-16 10:08:27 +0700

    Try find method with simple example

---
+++ 02-intermediate/01-array-method/10-find-method.js
@@ -0,0 +1,16 @@
+// ===== Example ====
+const products = [
+    { id: 1, name: "Laptop", price: 1500, stock: 0, category: "Electronics" },
+    { id: 2, name: "Book", price: 100, stock: 10, category: "Hoby" },
+    { id: 3, name: "Keyboard", price: 1000, stock: 0, category: "Electronics"},
+    { id: 4, name: "Shoes", price: 200, stock: 15, category: "Fashion"},
+    { id: 5, name: "Jacket", price: 500, stock: 0, category: "Fashion"}
+];
+
+// ---- FIND METHOD TO FIND SPECIFIC CASE ----
+
+// --- FIND SPECIFIC ID OF PRODUCTS ---
+const result = products.find(product => product.id === 4)
+console.log(result);
+
+// { id: 4, name: 'Shoes', price: 200, stock: 15, category: 'Fashion' }
