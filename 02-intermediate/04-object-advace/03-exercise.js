a3fb57975b5c2e6ed4082b88193cb0e7317ee7b7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-17 00:04:41 +0700

    feat: Create object with constructor function example

--- 02-intermediate/04-object-advace/03-exercise.js
+++ 02-intermediate/04-object-advace/03-exercise.js
@@ -1,5 +1,17 @@
 
 // Create bject
 function Buss(sopir,tujuan,penumpang,bayar){
-
+    this.sopir = sopir;
+    this.tujuan = tujuan;
+    this.penumpang = penumpang;
+    this.bayar = bayar;
 }
+
+const buss1 = new Buss("Bob",['jakarta','bekasi','jogja'],[],0);
+console.log(buss1);
+/* Buss {
+  sopir: 'Bob',
+  tujuan: [ 'jakarta', 'bekasi', 'jogja' ],
+  penumpang: [],
+  bayar: 0
+} */
