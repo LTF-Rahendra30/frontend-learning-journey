056afea266b1b9cf80abb9dab7ba318ff201d6b7
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-16 10:45:11 +0700

    feat: Add function with logic push in array And Logic if passenger is Undifined

--- 02-intermediate/exercise/03-bus-management.js
+++ 02-intermediate/exercise/03-bus-management.js
@@ -1,2 +1,23 @@
 // ============= bus passenger ============
 
+let passenger = [];
+const addPassager = function(name,psgr){
+    if(psgr){
+        psgr.push(name)
+        console.log(psgr);
+    } else{
+        for(let i = 0; i < psgr.length; i++){
+            if(psgr[i] === undefined){
+                psgr[i].push(name);
+                console.log(psgr);
+                
+            }
+        }
+    }
+}
+
+addPassager("Bob",passenger);
+addPassager(undefined,passenger);
+addPassager("B",passenger);
+// console.log(passenger);
+
