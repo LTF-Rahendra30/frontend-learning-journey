7218ea6b41f6b690bb001df009f7fe4aa87045a3
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-16 19:44:53 +0700

    Try crate object with Constructor function

--- 02-intermediate/04-object-advace/01-create-obj-func.js
+++ 02-intermediate/04-object-advace/01-create-obj-func.js
@@ -10,3 +10,16 @@ function createObject(name,email,city){
 
 console.log(createObject("Bob",true,"London"));
 // { name: 'Bob', email: true, city: 'London' }
+
+
+// ===== CREATE OBJECT WITH CONSTRUCTOR ====
+
+function AddUser(name,email,city){
+    this.name = name;
+    this.email = email;
+    this.city = city;
+}
+
+const user1 = new AddUser("Chloe",false,"Jogja");
+console.log(user1);
+// AddUser { name: 'Chloe', email: false, city: 'Jogja' }
