3de7c534cf5806dd69414ec3986914f06f3dc27a
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-19 16:52:13 +0700

    feat: Try dom selection with element by tag name and manipulation his bg color of paragraph

--- 03-dom-javascript/02-dom-selection/script.js
+++ 03-dom-javascript/02-dom-selection/script.js
@@ -3,3 +3,17 @@
 // ------- GET ELEMENT BY ID ------
 const judul = document.getElementById('judul');
 judul.style.color = 'red' // Change color of h1
+
+judul.innerHTML = 'Hidup Jokowi'
+
+
+// ====== GET ELEMENTS BY TAG NAME =====
+const paragraph = document.getElementsByTagName('p') // HTML Collection list
+// change bg color of paragraph
+paragraph[0].style.backgroundColor = 'pink'
+
+// with loop
+for (let i=0; i < paragraph.length; i++){
+    paragraph[i].style.backgroundColor = 'pink';
+
+}
