178d77868ec4d6b148710d90e92e6898b048a35a
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-20 17:07:58 +0700

    Try manipulation HTML content with querySelectorAll

--- 03-dom-javascript/02-dom-selection/query-selector.js
+++ 03-dom-javascript/02-dom-selection/query-selector.js
@@ -7,3 +7,13 @@ paragraph4.style.fontSize = '30px';
 
 const secondItem = document.querySelector('ul li:nth-child(2');
 secondItem.style.backgroundColor = 'orange';
+
+
+// ======== QUERY SELECTORS THAT RETURN Node LIST =====
+
+const p = document.querySelectorAll('p');
+
+// Simple Manipualation 
+for(let i = 0; i < p.length; i++){
+    p[i].innerHTML = "Lorem, ipsum";
+}
