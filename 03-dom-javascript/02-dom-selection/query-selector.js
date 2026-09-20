defa7c79a0218bafa53afb24cb4b5989a74886a5
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-20 17:02:20 +0700

    Try simple manipulation with query selectors

---
+++ 03-dom-javascript/02-dom-selection/query-selector.js
@@ -0,0 +1,9 @@
+// ======= QUERY  SELECTOR =======
+
+const paragraph4 = document.querySelector('#b p');
+
+paragraph4.style.color = 'pink';
+paragraph4.style.fontSize = '30px';
+
+const secondItem = document.querySelector('ul li:nth-child(2');
+secondItem.style.backgroundColor = 'orange';
