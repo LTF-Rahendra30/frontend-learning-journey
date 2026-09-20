1c8470ab0e50c08de511462c1472ee66d8611572
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-20 22:44:46 +0700

    feat: try InnerHTML and change content in html

---
+++ 03-dom-javascript/03-dom-manipulation/script1.js
@@ -0,0 +1,12 @@
+
+
+
+// ======== INNER HTML (CHANGE CONTENT OF HTML) FROM JS ==========
+
+// Change h1
+const h1 = document.getElementById('judul');
+judul.innerHTML = "HIDUP JOKOWIIIII";
+
+// change section A
+const sectionA = document.querySelector('#a');
+sectionA.innerHTML = 'Lorem ipsum';
