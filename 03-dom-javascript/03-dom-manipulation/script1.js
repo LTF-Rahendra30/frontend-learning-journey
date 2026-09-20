eb7b7305d8eb92fa634aebf211e712c7ff06c40c
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-20 22:49:28 +0700

    feat: Try change element style

--- 03-dom-javascript/03-dom-manipulation/script1.js
+++ 03-dom-javascript/03-dom-manipulation/script1.js
@@ -8,5 +8,12 @@ const h1 = document.getElementById('judul');
 judul.innerHTML = "HIDUP JOKOWIIIII";
 
 // change section A
-const sectionA = document.querySelector('#a');
-sectionA.innerHTML = 'Lorem ipsum';
+// const sectionA = document.querySelector('#a');
+// sectionA.innerHTML = 'Lorem ipsum';
+
+
+// ======= ELEMENT STYLE ======
+
+// Change color of first paragraph
+const paragraph1 = document.querySelectorAll('p')[0];
+paragraph1.style.color = 'red';
