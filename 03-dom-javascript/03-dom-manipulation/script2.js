6cfbb4c3bc4e993c5865af40d08ff41c5a4d7a41
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-21 16:53:37 +0700

    feat: Add functionality to create and append a new paragraph element to section A

--- 03-dom-javascript/03-dom-manipulation/script2.js
+++ 03-dom-javascript/03-dom-manipulation/script2.js
@@ -6,3 +6,10 @@ const newParagraph = document.createElement('p');
 
 // ------- CREATE NEW TEXT -----------
 const newText = document.createTextNode('This is new Paragraph');
+
+// ------ SAVE TEXT IN PARAGRAPH ----
+newParagraph.appendChild(newText);
+
+// ----- SAVE NEW PARAGRAPH IN ENDING SECTION OF A ---------
+const sectionA = document.getElementById('a');
+sectionA.appendChild(newParagraph);
