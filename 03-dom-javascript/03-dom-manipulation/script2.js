39986a0f0a997c6fb3ec36ad7ffad7fc2424753e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-21 16:50:28 +0700

    feat: Try create ne element and New content and stored in variable (without manipulation)

---
+++ 03-dom-javascript/03-dom-manipulation/script2.js
@@ -0,0 +1,8 @@
+// ======= DOM NODE MANIPULATION =========
+
+// -------- CREATE ELEMENT ---------
+
+const newParagraph = document.createElement('p');
+
+// ------- CREATE NEW TEXT -----------
+const newText = document.createTextNode('This is new Paragraph');
