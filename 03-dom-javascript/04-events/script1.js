2de574ae9a3ecb34f1bafb60c765612358dffaa6
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-23 16:35:33 +0700

    feat: try add new item list when p4 click with eventsListener method

--- 03-dom-javascript/04-events/script1.js
+++ 03-dom-javascript/04-events/script1.js
@@ -13,3 +13,18 @@ function changeColor(){
 // ====== CHANGE COLOR BY METHOD ====
 const p2 = document.querySelector('.p2');
 p2.onclick = changeColor;
+
+
+
+// ==== ADDEVENTSLISTENER ========
+// When p4 click, add new item
+
+const p4 = document.querySelector('.p4');
+const itemList = document.querySelector('section#b ul');
+
+p4.addEventListener('click', function(){
+    const newItem = document.createElement('li');
+    newItem.textContent = "item2";
+    itemList.append(newItem);
+
+})
