73bef6689a9f45c62d8024e7c3fad40718d223aa
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-23 22:43:56 +0700

    feat: Add eventListener to add task in task list and manipulation DOM

---
+++ project/02-todo-list/script.js
@@ -0,0 +1,22 @@
+const taskButton = document.getElementById('submit-button');
+const emptyStete = document.getElementById('emptyState');
+const taskList = document.getElementById('taskList');
+const nameTask = document.getElementById('name');
+
+
+taskButton.addEventListener('click', function(event){
+    event.preventDefault();
+
+    const newtask = document.createElement('div');
+    const nameValue = nameTask.value.trim();
+
+    newtask.innerHTML = `
+        <span class="task-text">${nameValue}</span>
+        <button class="remove-task-button">Hapus Tugas</button>
+    `
+    newtask.classList.add('task-item')
+    emptyStete.remove()
+
+    taskList.append(newtask);
+    
+})
