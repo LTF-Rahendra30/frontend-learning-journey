548986537c4c091bf1396810a05049756163ce5e
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-23 16:21:11 +0700

    Try inline html atribute Events to change bgcolor when click

---
+++ 03-dom-javascript/04-events/script1.js
@@ -0,0 +1,9 @@
+// ======= CHANGE BG COLOR OF P3 WHEN CLICK ======
+
+const p3 = document.querySelector('.p3');
+
+// ==== INLINE HTML ATRIBUTE ====
+
+function changeColor(){
+    p3.style.backgroundColor = 'lightgreen'
+}
