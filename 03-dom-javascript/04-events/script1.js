f13e837ac6863593389e6652cba7ced7fcfa1630
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-23 16:25:21 +0700

    Try change color by method and add classlist style

--- 03-dom-javascript/04-events/script1.js
+++ 03-dom-javascript/04-events/script1.js
@@ -5,5 +5,11 @@ const p3 = document.querySelector('.p3');
 // ==== INLINE HTML ATRIBUTE ====
 
 function changeColor(){
-    p3.style.backgroundColor = 'lightgreen'
+    p3.style.backgroundColor = 'lightgreen';
+    p2.classList.add('color')
 }
+
+
+// ====== CHANGE COLOR BY METHOD ====
+const p2 = document.querySelector('.p2');
+p2.onclick = changeColor;
