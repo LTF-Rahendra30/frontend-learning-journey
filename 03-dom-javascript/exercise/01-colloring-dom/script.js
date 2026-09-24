db67ea8f2b830ac4ae3a91a964ad92c58cb158e6
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-24 13:46:09 +0700

    feat: Add random button for change bgcolor

--- 03-dom-javascript/exercise/01-colloring-dom/script.js
+++ 03-dom-javascript/exercise/01-colloring-dom/script.js
@@ -1,8 +1,17 @@
 const button = document.getElementById('ubahWarna');
-// const bodyBackground = document.getElementById('body');
-
+const body = document.getElementById('body');
+const containerButton = document.querySelector('.container')
 
 button.addEventListener('click', ()=> {
     document.body.classList.toggle('bg-color')
 
-})
+});
+
+
+
+// ==== RANDOM BUTTON TO CHANGE BG COLOR OF BODY ====
+
+const randomBtn = document.createElement('button');
+randomBtn.textContent = 'Random Bg Warna';
+
+containerButton.append(randomBtn);
