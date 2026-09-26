cf11b20def3b297be3a0c51845baa91b2d86dc4f
LTF-Rahendra30 <kecuu91@gmail.com>
2026-09-26 13:54:26 +0700

    feat: Encapsulate random choose with a function

--- 03-dom-javascript/exercise/03-minigame2/script.js
+++ 03-dom-javascript/exercise/03-minigame2/script.js
@@ -1,13 +1,19 @@
-const komputerChose = ['gajah','orang','semut'];
-
-const indeksAcak = Math.floor(Math.random() * komputerChose.length);
-
-// 3. Ambil data berdasarkan indeks tersebut
-const randomChoose = komputerChose[indeksAcak];
-
-console.log(randomChoose);
 
 
+function randomChoose(){
+    const komputerChose = ['gajah','orang','semut'];
+    
+    const indeksAcak = Math.floor(Math.random() * komputerChose.length);
+    
+    // 3. Ambil data berdasarkan indeks tersebut
+    const randomChoose = komputerChose[indeksAcak];
+    
+    return randomChoose;
+}
+
+console.log(randomChoose());
+console.log(randomChoose());
+console.log(randomChoose());
 
 // const komputerUI = document.querySelector('.komputer');
 // komputer.textContent += 'Semut'
